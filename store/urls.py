30d8c4f7e0404c61dcbb79a736d95e5a85028835
Ovalga <145358551+Ovalga@users.noreply.github.com>
2025-05-30 23:10:37 +0400

    Лабораторная работа 4 еще чуть чуть дополнила

--- store/urls.py
+++ store/urls.py
@@ -42,5 +42,7 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
 
